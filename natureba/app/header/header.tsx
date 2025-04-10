e4b29ca14c30944ddd0f0fee7544755f57b6e9be
Math2359 <64596723+Math2359@users.noreply.github.com>
2025-04-10 22:15:26 +0000

    Adicinando Home page

--- natureba/app/header/header.tsx
+++ natureba/app/header/header.tsx
@@ -4,14 +4,14 @@ import { Link } from "react-router"
 export const Header = () => {
 
     return (
-        <nav className="flex items-center w-full">
+        <nav className="flex items-center w-full border-b-1 border-b-[#3E7D4E] fixed">
             <Link to="/">
                 <img src={logo_verde} />
             </Link>
-            <div className="ml-10">
-                <Link to="/loja">Loja</Link>
-                <Link to="#">Blog</Link>
-                <Link to="/loja">Sobre</Link>
+            <div className="ml-30 text-[18px]">
+                <Link className="mr-15 decoration-[#8B5A2B] underline-offset-10 hover:text-[#B8B8B8] hover:underline" to="/loja">Loja</Link>
+                <Link className="mr-15 decoration-[#8B5A2B] underline-offset-10 hover:text-[#B8B8B8] hover:underline" to="#">Blog</Link>
+                <Link className="decoration-[#8B5A2B] underline-offset-10 hover:text-[#B8B8B8] hover:underline" to="/loja">Sobre</Link>
 
             </div>
         </nav>
--- natureba/app/welcome/welcome.tsx
+++ natureba/app/welcome/welcome.tsx
@@ -1,50 +1,37 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
+import imagem_home from "public/imagem_home.svg";
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
+    <main className="w-full h-dvh">
+      <section className="w-full grid grid-flow-col grid-cols-2 h-full" style={styles.homePage}>
+        <div className="col-span-1 flex items-center justify-center">
+          <div className="text-[#8B5A2B] flex flex-col text-[80px]/20 font-[Judson] m-0">
+            <p><span className="text-[#3E7D4E]">na</span>tural</p>
+            <p><span className="text-[#3E7D4E]">tu</span>rbinado</p>
+            <p><span className="text-[#3E7D4E]">re</span>sponsável</p>
+            <p><span className="text-[#3E7D4E]">ba</span>ratíssimo</p>
           </div>
-        </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
-            </p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
         </div>
-      </div>
+          
+      </section>
     </main>
   );
 }
+
+const styles = {
+  homePage: {
+    backgroundImage: `url(${imagem_home})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "right",
+    backgroundSize: "auto 90%"
+  },
+  textoHome: {
+    fontSize: "80px",
+    fontFamily: 'Judson',
+    color: '#8B5A2B'
+  }
+}
+
 
 const resources = [
   {
