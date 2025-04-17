6312d7132193a2c46f46ad96c383177df3d34950
José Vítor <cavalcanti.josevitor@gmail.com>
2025-04-17 00:18:35 +0000

    Adicionando lista no footer

--- natureba/app/footer/footer.tsx
+++ natureba/app/footer/footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router"
 import instagram_logo from "/instagram_logo.svg";
 import whatsapp_logo from "/whatsapp_logo.svg";
 import twitter_logo from "/twitter_logo.svg";
@@ -6,9 +7,31 @@ import info_endereco from "/info_endereco.svg";
 export const Footer = () => {
     return (
         <footer className="bg-[#3E7D4E] text-white flex justify-between items-center p-4">
-            <div className="flex-1 text-center">
-                <p>Column 1</p>
+            <div className="flex-1 flex justify-center  " >
+                <ul>
+                    <li><Link className="" to='#blog'>Blog</Link></li>
+                    <li><Link className="" to='/loja'>Loja</Link></li>
+                    <ul> <span className="text-[13px] ">
+                        <li>Alimentos</li>
+                        <li>Cosméticos</li>
+                        <li>Suplementos</li>
+                    </span> </ul>
+                    <li><Link className="" to='#sobre'>Sobre</Link></li>
+                </ul>
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
             <div className="w-px bg-white h-16"></div>
             <div className="flex-1 flex justify-center space-x-4">
                 <a href="https://www.instagram.com" target="_blank">
